3df5294a68a540c1c9966d69bb3a2e8ad2a41c57
Matias Roel <matiasroel241201@gmail.com>
2022-06-21 18:29:29 -0300

    Carrito terminado sin validacion

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,13 +2,14 @@ import { useCartContext } from "../../context/CartContext"
 import React, { useState } from "react"
 import { addDoc, collection, getFirestore, query, where, documentId, writeBatch, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const Cart = () => {
 
     const { cartList,removeItem,precioTotal,vaciarCarrito } = useCartContext()
     const [dataForm, setDataForm] = useState({ email: '', phone: '', name:'' })
   
-     async function generarOrden (){
+     async function generarOrden (e){
 
       let orden = {}
 
@@ -23,7 +24,8 @@ const Cart = () => {
 
           return {id,nombre,precio}
       }) 
-      // CREAR
+      
+      
 
       const db = getFirestore()
         const queryCollection = collection(db, 'orders')
@@ -46,7 +48,13 @@ const Cart = () => {
         .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
               stock: res.data().stock - cartList.find(item => item.id === res.id).cantidad
         }) ))
-        .finally(()=> console.log('actualizado'))
+        .finally(()=> Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Su compra se realizo con exito',
+            showConfirmButton: false,
+            timer: 2000
+          }))
 
         batch.commit()
     }
@@ -59,10 +67,10 @@ const Cart = () => {
   }
 
 
-
     return(
         <div>
             <div> 
+          
               {cartList.length === 0 ? 
                   <div className="cartnot">
                       <label className="cartnot__title">Usted no tiene productos en el carrito</label>
@@ -72,81 +80,78 @@ const Cart = () => {
                   </div>
               :
                   <>
-                                                  <div className="cartItems"  >
-                                                      <h2 className="cartItems__title">Sus compras:</h2>
-                                                      <div className="cartItems__contenedor">
-                                                      {cartList.map(product =>
-                                                      
+                  
+                    <div className="cartItems" >
+                        <div>
+                            <h2 className="cartItems__title">Carrito <i className="fas fa-shopping-cart icono-carrito"></i></h2>
+                            <div className="cartItems__contenedor">
+                                {cartList.map(product =>
                                                         <div className="cartItems__contenedor-item" key={product.id}>
-                                                          <img className="cartItems__img" src={product.url}/>
-                                                          <h5 className="cartItems__info-destino"><span>Destino:</span> {product.name}</h5>
-                                                          <h5 className="cartItems__info-precio"><span>Precio Unitario: </span>${product.precio}</h5>
-                                                          <h5 className="cartItems__info-cantidad"><span>Cantidad de pasajes: </span>  {product.cantidad}</h5>
-                                                          {/* <button className="btn-count btn-count--red" onClick={removeItem}>-</button>
-                                                          <button className="btn-count" onClick={addToCart}>+</button> */}
-                                                          <h5 className='cartItems__x' onClick={()=> removeItem(product.id)}>X</h5> 
-                                                          <div className="precioTotal">
-                                                            <h2 className="cartItems__total">Total: <span>${precioTotal()}</span></h2> 
-                                                          </div>
+                                                            <img className="cartItems__img" src={product.url}/>
+                                                            <h5 className="cartItems__info-destino"><span>Destino:</span> {product.name}</h5>
+                                                            <h5 className="cartItems__info-precio"><span>Precio Unitario: </span>${product.precio}</h5>
+                                                            <h5 className="cartItems__info-cantidad"><span>Cantidad de pasajes: </span>  {product.cantidad}</h5>
+                                                            <h5 className='cartItems__x' onClick={()=> removeItem(product.id)}>X</h5> 
+                                                            <div className="precioTotal">
+                                                                <button onClick={vaciarCarrito} className='vaciarCarrito'>Vaciar carrito
+                                                                    <i className="fa-solid fa-trash icono-basura"></i>
+                                                                </button>
+                                                                <h2 className="cartItems__total">Total: <span>${precioTotal()}</span></h2> 
+                                                            </div>
                                                         </div>
-                                                      
-                                                        )}
-                                                      </div>
-                                                      <div className="container__btnCart">
-                                                        <button onClick={vaciarCarrito} className='cartnot__select'>Vaciar carrito</button>
-                                                        <button  className="cartnot__select"  onClick={generarOrden} >Terminar Compra</button>
-                                                      </div>
-                                                    </div>  
-                                                  
-                      
+                                                        
+                                                            )}
+                            </div>
+                        </div>
+                        <div className='form'>
+                        <form 
+                            
+                            onSubmit={generarOrden}         
+                        >
+                            <div className="contenedor__form">
+                            <h5 className="titulo__formulario">Ingrese sus datos: </h5>                
+                            <input 
+                                className='form-control'
+                                type='text' 
+                                name='name' 
+                                placeholder='Ingrese el nombre' 
+                                value={dataForm.name}
+                                onChange={handlerChange}
+                            />
+                            <input 
+                                className='form-control'
+                                type='text' 
+                                name='phone'
+                                placeholder='Ingrese el telefono' 
+                                value={dataForm.phone}
+                                onChange={handlerChange}
+                            />
+                            <input 
+                                className='form-control'
+                                type='email' 
+                                name='email'
+                                placeholder='Ingrese el email' 
+                                value={dataForm.email}
+                                onChange={handlerChange}
+                            />
+                            <input 
+                                className='form-control'
+                                type='email' 
+                                name='email1'
+                                placeholder='repita email' 
+                                value={dataForm.email}
+                                onChange={handlerChange}
+                            />
+                            </div>
+                        </form>
+                            <button className="cartnot__select cartnot__select--finalizar"  onClick={generarOrden} >Terminar Compra
+                                <i className="fa-solid fa-bag-shopping icono-bolso" onClick={generarOrden}></i>
+                            </button>
+                        </div>
+                    </div>  
                     
                   </>
               }
-
-{ cartList.length !== 0 &&
-                <center>
-                    <form 
-                        className='form'
-                        onSubmit={generarOrden}         
-                        >
-                        <h5 className="titulo__formulario">Formulario: </h5>                
-                        <input 
-                            className='form-control'
-                            type='text' 
-                            name='name' 
-                            placeholder='Ingrese el nombre' 
-                            value={dataForm.name}
-                            onChange={handlerChange}
-                        /><br />
-                        <input 
-                            className='form-control'
-                            type='text' 
-                            name='phone'
-                            placeholder='Ingrese el telefono' 
-                            value={dataForm.phone}
-                            onChange={handlerChange}
-                        /><br/>
-                        <input 
-                            className='form-control'
-                            type='email' 
-                            name='email'
-                            placeholder='Ingrese el email' 
-                            value={dataForm.email}
-                            onChange={handlerChange}
-                        /><br/>
-                        <input 
-                            className='form-control'
-                            type='email' 
-                            name='email1'
-                            placeholder='repita email' 
-                            value={dataForm.email}
-                            onChange={handlerChange}
-                        /><br/>
-                    </form>
-                </center>
-
-            }
-
             </div>
         </div> 
     ) 
